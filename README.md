c5c127aaeb6280fd677d1c120c0fd4f1c141769c
angelocarly <angelo.carly@protonmail.com>
2020-09-22 22:42:10 +0200

    Add link and follow commands

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # listentothissy
 Discord bot that creates spotify playlists based on discord messages
+
+## Configuration
+This bot requires a few necessary environment variables in order to function. These are
+- DISCORD_TOKEN - Discord bot token
+- CLIENT_ID - spotify client id
+- CLIENT_SECRET - spotify client secret
+- REDIRECT_URI - spotify redirect uri
+
+## Usage
+
+### Link account
+- Get an authorization url by sending `~link`
+- Visit the url and authorize the bot
+- Send the following url to the bot using `~link <url>`
--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,42 @@
+use std::collections::HashMap;
 use std::env;
 use std::sync::{Arc, Mutex};
 
 use rspotify::client::Spotify;
 use rspotify::oauth2::{SpotifyClientCredentials, SpotifyOAuth};
-use rspotify::util::get_token;
+use rspotify::util::{generate_random_string, get_token, process_token, request_token};
 use serenity::async_trait;
 use serenity::client::{Client, Context, EventHandler};
-use serenity::framework::standard::{
-    CommandResult,
-    macros::{
-        command,
-        group,
-    },
-    StandardFramework,
-};
-use serenity::model::channel::Message;
+use serenity::framework::standard::{Args, CommandResult, macros::{
+    command,
+    group,
+}, StandardFramework};
+use serenity::model::channel::{Message, PrivateChannel};
 use serenity::prelude::TypeMapKey;
+use std::collections::hash_map::RandomState;
+
+struct SubscribeData {
+    discord_user: u64,
+    spotify_user: String,
+    spotify_channel: String
+}
 
 #[group]
-#[commands(add, sync)]
+#[commands(link, follow)]
 struct General;
 
 struct Handler;
 
+// A mapping of discord user id's to spotify objects
 struct SpotifyContainer;
-
 impl TypeMapKey for SpotifyContainer {
-    type Value = Arc<Spotify>;
+    type Value = HashMap<u64, Spotify>;
+}
+
+// A mapping of discord channels to subscribe data
+struct SubscribeContainer;
+impl TypeMapKey for SubscribeContainer {
+    type Value = HashMap<u64, SubscribeData>;
 }
 
 #[async_trait]
@@ -34,23 +44,6 @@ impl EventHandler for Handler {}
 
 #[tokio::main]
 async fn main() {
-    // Spotify setup
-    let spotify;
-    let mut oauth = SpotifyOAuth::default()
-        .scope("playlist-modify-public")
-        .build();
-    match get_token(&mut oauth).await {
-        Some(token_info) => {
-            let client_credential = SpotifyClientCredentials::default()
-                .token_info(token_info)
-                .build();
-            spotify = Spotify::default()
-                .client_credentials_manager(client_credential)
-                .build();
-            println!("Auth succeeded");
-        }
-        None => panic!("auth failed"),
-    };
 
     // Discord setup
     let framework = StandardFramework::new()
@@ -66,7 +59,8 @@ async fn main() {
         .expect("Error creating client");
     {
         let mut data = client.data.write().await;
-        data.insert::<SpotifyContainer>(Arc::new(spotify));
+        data.insert::<SpotifyContainer>(HashMap::new());
+        data.insert::<SubscribeContainer>(HashMap::new());
     }
 
     // start listening for events by starting a single shard
@@ -75,33 +69,143 @@ async fn main() {
     }
 }
 
+// #[command]
+// async fn add(ctx: &Context, msg: &Message) -> CommandResult {
+//
+//     // Retrieve spotify context
+//     let data = ctx.data.read().await;
+//     let spotify = data.get::<SpotifyContainer>().expect("Expected SpotifyContainer in context");
+//
+//     let url = msg.content.split_whitespace().nth(1).unwrap_or("");
+//     if url.starts_with("https://open.spotify.com/track/") {
+//         let track_data = spotify.track(url).await;
+//         let track_id = url.split('/').last().unwrap().split('?').next().unwrap();
+//         let track_uri = format!("spotify:track:{}", track_id);
+//
+//         let name = track_data.unwrap().name;
+//         println!("Adding {} to playlist", url);
+//         let profile = env::var("SPOTIFY_PROFILE").expect("Expected spotify profile");
+//         let playlist = env::var("SPOTIFY_PLAYLIST").expect("Expected spotify playlist");
+//         let res = spotify.user_playlist_add_tracks(&*profile, &*playlist, &*vec![track_uri], Option::from(0)).await;
+//         msg.reply(ctx, format!("Added {:?} to playlist", name)).await?;
+//         println!("{:?}", res);
+//     }
+//
+//     Ok(())
+// }
+
 #[command]
-async fn add(ctx: &Context, msg: &Message) -> CommandResult {
+async fn follow(ctx: &Context, msg: &Message, args: Args) -> CommandResult {
 
-    // Retrieve spotify context
-    let data = ctx.data.read().await;
-    let spotify = data.get::<SpotifyContainer>().expect("Expected SpotifyContainer in context");
+    let mut data = ctx.data.write().await;
+    let map = data.get::<SpotifyContainer>().expect("Expected SpotifyContainer in context");
 
-    let url = msg.content.split_whitespace().nth(1).unwrap_or("");
-    if url.starts_with("https://open.spotify.com/track/") {
-        let track_data = spotify.track(url).await;
-        let track_id = url.split('/').last().unwrap().split('?').next().unwrap();
-        let track_uri = format!("spotify:track:{}", track_id);
+    match map.get(msg.author.id.as_u64()) {
+        Some(spotify) => {
 
-        let name = track_data.unwrap().name;
-        println!("Adding {} to playlist", url);
-        let res = spotify.user_playlist_add_tracks("31yxo4oikta6ebumsj26ivlanwji", "0j2PpLzQD4d1y3ZkeJFVqa", &*vec![track_uri], Option::from(0)).await;
-        msg.reply(ctx, format!("Added {:?} to playlist", name)).await?;
-        println!("{:?}", res);
+            // Verify if playlist is the correct format
+            let mut playlist = args.message().to_string();
+            if !playlist.starts_with("spotify:playlist:") {
+                msg.author.dm(&ctx.http, |m| m.content("Your playlist URI is required, this is in the format spotify:playlist:xxxxxxxx")).await?;
+                return Ok(());
+            }
+
+            // Verify if user owns the playlist
+            let spotify_id = spotify.me().await.unwrap().id;
+            let playl = spotify.user_playlist(&spotify_id, Some(&mut playlist), None, None).await.unwrap().id;
+            if !playlist.ends_with(playl.as_str()) {
+                msg.author.dm(&ctx.http, |m| m.content("You do not own this playlist")).await?;
+                return Ok(());
+            }
+
+            // Insert subscribe data
+            let subscribe = SubscribeData {
+                discord_user: msg.author.id.0,
+                spotify_user: spotify_id,
+                spotify_channel: "".to_string()
+            };
+
+            let mut map = data.get_mut::<SubscribeContainer>().expect("Expected subscription hashmap");
+            map.insert(msg.channel_id.0, subscribe);
+
+            msg.author.dm(&ctx.http, |m| m.content("Succesfully followed text channel!")).await?;
+        },
+        None => {
+            msg.author.dm(&ctx.http, |m| m.content("Your account is not linked yet. Message ~link to link it.")).await?;
+        }
     }
+
 
     Ok(())
 }
 
+/*
+ * Link spotify account to discord id
+ */
 #[command]
-async fn sync(ctx: &Context, msg: &Message) -> CommandResult {
+async fn link(ctx: &Context, msg: &Message, args: Args) -> CommandResult {
 
-    println!("OK");
+    if args.message().is_empty() {
 
-        Ok(())
+        // ~link command was empty, check if id is already linked
+        let data = ctx.data.read().await;
+        let cache = data.get::<SpotifyContainer>().expect("Expected SpotifyContainer in context");
+
+        match cache.get(&msg.author.id.0) {
+            Some(_) => {
+                msg.author.dm(&ctx.http, |m| m.content("Your account is already linked")).await?;
+            }
+            None => {
+                // Craft an authorization url and dm it to the user
+                let oauth = SpotifyOAuth::default()
+                    .scope("playlist-modify-public")
+                    .build();
+
+                let state = generate_random_string(16);
+                let auth_url = oauth.get_authorize_url(Some(&state), None);
+                let message = format!("In order to link your account, go to the following url, allow access and send me the final url like this: ~link <url>\n{}", auth_url);
+                msg.author.dm(&ctx.http, |m| m.content(message)).await?;
+
+            }
+        }
+    }
+    else {
+
+        // A message was given with the ~link command, try to complete the authorization
+        let mut oauth = SpotifyOAuth::default()
+            .scope("playlist-modify-public")
+            .build();
+
+        let mut url: String = args.message().to_string();
+        match process_token(&mut oauth, &mut url).await {
+            Some(token_info) => {
+
+                // Building the client credentials with the access token.
+                let client_credential = SpotifyClientCredentials::default()
+                    .token_info(token_info)
+                    .build();
+
+                // Initialize the Spotify client.
+                let spotify = Spotify::default()
+                    .client_credentials_manager(client_credential)
+                    .build();
+
+                let user = spotify.current_user().await.unwrap();
+                let welcome_msg = format!("Welcome {}, your account is linked!", user.display_name.unwrap_or(user.id));
+
+                // Store spotify in context
+                let mut data = ctx.data.write().await;
+                let mut map = data.get_mut::<SpotifyContainer>().expect("Expected spotify hashmap");
+                map.insert(msg.author.id.0, spotify);
+
+                msg.author.dm(&ctx.http, |m| m.content(welcome_msg)).await?;
+                println!("Linked new account, id {}", msg.author.id.0);
+            },
+            None => {
+                msg.author.dm(&ctx.http, |m| m.content("Failed to link account, did you enter the correct url? Try again by sending ~link")).await?;
+            }
+        }
+    }
+
+    Ok(())
 }
